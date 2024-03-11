4e1b898a971e3e1f9749e87934f0d89f9a7b10c0
inaDAGod <danialee14@gmail.com>
2024-03-11 19:48:37 -0400

    modificacion login, error rebalse

--- traso/lib/login/login_page.dart
+++ traso/lib/login/login_page.dart
@@ -20,80 +20,82 @@ class LoginPage extends StatelessWidget {
       backgroundColor: const Color(0xFFD6D7FE),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-             Image.asset('lib/images/medusa.webp',
-             ),
-
-              const SizedBox(height: 10),
-
-              // welcome back, you've been missed!
-              const Text(
-                'Bienvenidx de nuevo!',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 4, 4, 4),
-                  fontSize: 30,
-                  
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+          
+               Image.asset('lib/images/medusa.webp',
+               ),
+          
+                const SizedBox(height: 30),
+          
+                // welcome back, you've been missed!
+                const Text(
+                  'Bienvenidx de nuevo!',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 4, 4, 4),
+                    fontSize: 30,
+                    
+                  ),
                 ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // username textfield
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Correo electronico',
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              // password textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Contraseña',
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 25),
-
-              
-
-              // sign in button
-              MyButton(
-                onTap: signUserIn,
-              ),
-
-              const SizedBox(height: 30),
-
-              // not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'No estas registradx?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  
-                  
-                ],
-              ),
-              const SizedBox(height: 4),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Registrate ahora',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
+          
+                const SizedBox(height: 25),
+          
+                // username textfield
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'Correo electronico',
+                  obscureText: false,
+                ),
+          
+                const SizedBox(height: 15),
+          
+                // password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Contraseña',
+                  obscureText: true,
+                ),
+          
+                const SizedBox(height: 25),
+          
+                
+          
+                // sign in button
+                MyButton(
+                  onTap: signUserIn,
+                ),
+          
+                const SizedBox(height: 30),
+          
+                // not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No estas registradx?',
+                      style: TextStyle(color: Colors.grey[700]),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    
+                    
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Registrate ahora',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
