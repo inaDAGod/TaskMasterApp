dd51ca446a080c77ebd6210d5a61c49bb5d7643a
JoshNisth <134982802+JoshNisth@users.noreply.github.com>
2024-03-13 22:10:16 -0400

    Solucion de visualizacion

--- traso/lib/añadirTarea.dart
+++ traso/lib/añadirTarea.dart
@@ -33,147 +33,155 @@ class _AnadirTareaState extends State<AnadirTarea> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFF4BA),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  icon: Icon(Icons.close),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(Icons.close),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Expanded(
-              child: Center(
-                child: Text(
-                  'AÑADIR',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 4, 4, 4),
-                    fontSize: 60,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Times new roman',
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              "Titulo:",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              // Textbox para escribir el título de la tarea
-              decoration: InputDecoration(
-                hintText: 'Titulo de tu tarea',
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Categoria:",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(
-                hintText: 'Selecciona la categoría',
-              ),
-              // Combobox para seleccionar la categoría de la tarea
-              items: <String>['Categoria 1', 'Categoria 2', 'Categoria 3']
-                  .map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? value) {},
-            ),
-            SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Fin:",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      TextFormField(
-                        controller: _dateController,
-                        readOnly: true,
-                        onTap: () => _selectDate(context),
-                        decoration: InputDecoration(
-                          hintText: 'Selecciona una fecha',
-                          suffixIcon: Icon(Icons.calendar_today),
-                        ),
-                      ),
-                    ],
+                SizedBox(height: 50),
+                Center(
+                  child: Text(
+                    'AÑADIR',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 4, 4, 4),
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Times new roman',
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Descripcion:",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              // Textfield grande para escribir la descripción
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              decoration: InputDecoration(
-                hintText: 'Agregar descripción',
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.notifications), // Icono de la campana
-                SizedBox(width: 5),
+                SizedBox(height: 30),
                 Text(
-                  "Recuerdame:",
-                  style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+                  "Titulo:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(width: 5),
-                Checkbox(
-                  // Checkbox para recordar la tarea
-                  value: _recuerdame,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _recuerdame = value ?? false;
-                    });
+                TextFormField(
+                  // Textbox para escribir el título de la tarea
+                  decoration: InputDecoration(
+                    hintText: 'Titulo de tu tarea',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Categoria:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    hintText: 'Selecciona la categoría',
+                  ),
+                  // Combobox para seleccionar la categoría de la tarea
+                  items: <String>[
+                    'Estudio',
+                    'Personal',
+                    'Favoritas',
+                    'Otro (varios)'
+                  ].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {},
+                ),
+                SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Fin:",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          TextFormField(
+                            controller: _dateController,
+                            readOnly: true,
+                            onTap: () => _selectDate(context),
+                            decoration: InputDecoration(
+                              hintText: 'Selecciona una fecha',
+                              suffixIcon: Icon(Icons.calendar_today),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Descripcion:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  // Textfield grande para escribir la descripción
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Agregar descripción',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.notifications), // Icono de la campana
+                    SizedBox(width: 5),
+                    Text(
+                      "Recuerdame:",
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 5),
+                    Checkbox(
+                      // Checkbox para recordar la tarea
+                      value: _recuerdame,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _recuerdame = value ?? false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20), // Ajuste del espacio
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción para guardar la tarea
                   },
+                  child: Text('Guardar'),
                 ),
+                SizedBox(height: 20), // Ajuste del espacio
               ],
             ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                // Acción para guardar la tarea
-              },
-              child: Text('Guardar'),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
-
-
+//Solo era para correrlo directo sin usaar el main jiji
+/*
 void main() {
   runApp(MaterialApp(
     home: AnadirTarea(),
   ));
 }
+*/
